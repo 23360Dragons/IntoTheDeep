d0b1697815998f195b08fa8aac9fbb48f2e7272b
UnheardWolf344 <griffin3868@gmail.com>
2024-10-08 19:52:50 -0400

    add comments and fix wrong object call in DriveMotor.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/utils/DriveMotor.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/utils/DriveMotor.java
@@ -1,21 +1,17 @@
 package org.firstinspires.ftc.teamcode.TeleOp.utils;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 
 public class DriveMotor {
     public static DcMotorEx createNewMotor (HardwareMap hardwareMap, String name) {
-
         DcMotorEx dcMotor;
 
-        dcMotor = hardwareMap.get(DcMotorEx.class, name);
+        dcMotor = hardwareMap.get(DcMotorEx.class, name); // gets a dcMotorEx object of the name "name"
         dcMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        dcMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        dcMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
 
-        return dcMotor;
+        return dcMotor; // returns a DcMotorEx object
     }
 }
