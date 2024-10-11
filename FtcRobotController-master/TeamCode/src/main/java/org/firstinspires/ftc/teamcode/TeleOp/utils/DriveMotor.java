b7a1b9c2337ecf34a9c89724fc545b3bb8c54dac
UnheardWolf344 <griffin3868@gmail.com>
2024-10-11 19:13:05 -0400

    fix comments

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/utils/DriveMotor.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/utils/DriveMotor.java
@@ -9,10 +9,10 @@ public class DriveMotor {
     public static DcMotor newMotor(HardwareMap hardwareMap, String name) {
         DcMotor dcMotor;
 
-        dcMotor = hardwareMap.get(DcMotor.class, name); // gets a dcMotorEx object of the name "name"
+        dcMotor = hardwareMap.get(DcMotor.class, name); // gets a dcMotor object of the name "name"
         dcMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         dcMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        return dcMotor; // returns a DcMotorEx object
+        return dcMotor; // returns a DcMotor object
     }
 }
