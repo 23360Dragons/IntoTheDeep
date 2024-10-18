91f0a2b488068c58ef14c6cab7317f5a16643315
23360Dragons <23360Dragons@gmail.com>
2024-10-18 17:16:28 -0400

    Update DriveMotor.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/init/DriveMotor.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/init/DriveMotor.java
@@ -50,46 +50,71 @@ public class DriveMotor {
             }
 
         }
+    }
 
+    public static void initialize2 (HardwareMap hardwareMap) {
+        DcMotor[] motors = {leftFront, leftBack, rightFront, rightBack};
 
-        /*try {
-            leftFront = hardwareMap.get(DcMotor.class, "leftFront"); // gets a dcMotor object of the name "name"
-            leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            leftFront.setDirection(DcMotor.Direction.REVERSE); // fix faulty drive behavior 10/15
+        int i=0;
+        try
+        {
+            for (DcMotor m : motors)
+            {
+                //left front
+                try{
+                    leftFront = hardwareMap.get(DcMotor.class, "leftFront"); // gets a dcMotor object of the name "name"
+                    leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                    leftFront.setDirection(DcMotor.Direction.REVERSE); // fix faulty drive behavior 10/15
+                }
+                catch(IllegalArgumentException e)
+                {
+                    InitInfo.exceptions.append("Configuration Error: ").append("leftFront").append(" does not exist").append("\n");
+                    InitInfo.exceptionOccurred = true;}
+                //right front
+                try
+                {
+                    rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+                    rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                }
+                catch(IllegalArgumentException e)
+                {
+                    InitInfo.exceptions.append("Configuration Error: ").append("rightFront").append(" does not exist").append("\n");
+                    InitInfo.exceptionOccurred = true;
+                }
+                //left back
+                try
+                {
+                    leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+                    leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                    leftBack.setDirection(DcMotor.Direction.REVERSE);
+                }
+                catch(IllegalArgumentException e)
+                {
+                    InitInfo.exceptions.append("Configuration Error: ").append("leftBack").append(" does not exist").append("\n");
+                    InitInfo.exceptionOccurred = true;}
+                //right back
+                try
+                {
+                    rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+                    rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                }
+                catch(IllegalArgumentException e)
+                {
+                    InitInfo.exceptions.append("Configuration Error: ").append("rightBack").append(" does not exist").append("\n");
+                    InitInfo.exceptionOccurred = true;
+                }
+            }
 
-        } catch (IllegalArgumentException e) {
-            InitInfo.exceptions.append("Configuration Error: ").append("leftFront").append(" does not exist").append("\n");
+        }
+        catch(Exception ex)
+        {
+            InitInfo.exceptions.append("Configuration Error: ").append("General Motor Config").append(ex.getMessage()).append("\n");
             InitInfo.exceptionOccurred = true;
         }
-
-        try {
-            rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-            rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        } catch (IllegalArgumentException e) {
-            InitInfo.exceptions.append("Configuration Error: ").append("rightFront").append(" does not exist").append("\n");
-            InitInfo.exceptionOccurred = true;
-        }
-
-        try {
-            leftBack = hardwareMap.get(DcMotor.class, "leftBack");
-            leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        } catch (IllegalArgumentException e) {
-            InitInfo.exceptions.append("Configuration Error: ").append("leftBack").append(" does not exist").append("\n");
-            InitInfo.exceptionOccurred = true;
-        }
-
-        try {
-            rightBack = hardwareMap.get(DcMotor.class, "rightBack");
-            rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        } catch (IllegalArgumentException e) {
-            InitInfo.exceptions.append("Configuration Error: ").append("rightBack").append(" does not exist").append("\n");
-            InitInfo.exceptionOccurred = true;
-        }*/
     }
 }
 
