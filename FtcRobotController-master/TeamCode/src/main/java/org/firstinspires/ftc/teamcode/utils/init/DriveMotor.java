94682ca9cab2b5af3f3809f36ebea9aa65820277
UnheardWolf344 <griffin3868@gmail.com>
2024-10-18 17:29:42 -0400

    drivemotor update

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/init/DriveMotor.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/init/DriveMotor.java
@@ -19,6 +19,35 @@ public class DriveMotor {
     public static boolean isValid;
 
     public static void initialize (HardwareMap hardwareMap) {
+        for (int i = 0; i < driveMotors.length - 1; i++)
+        {
+            try {
+                driveMotors[i] = hardwareMap.get(DcMotor.class, driveMotorNames[i]);
+                driveMotors[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                driveMotors[i].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                if (i % 2 == 0)
+                    driveMotors[i].setDirection(DcMotor.Direction.REVERSE);
+
+                switch (i) {
+                    case 0: {
+                        leftFront = driveMotors[i];
+                    }
+                    case 1: {
+                        rightFront = driveMotors[i];
+                    }
+                    case 2: {
+                        leftBack = driveMotors[i];
+                    }
+                    case 3: {
+                        driveMotors[3] = driveMotors[i];
+                    }
+                }
+            } catch (IllegalArgumentException e) {
+                InitInfo.exceptions.append("Configuration Error: ").append(driveMotorNames[i]).append(" does not exist").append("\n");
+                InitInfo.exceptionOccurred = true;
+                isValid = false;
+            }
+        /*
         int i=0;
         for (DcMotor m : driveMotors) {
             try {
@@ -52,7 +81,7 @@ public class DriveMotor {
                 isValid = false;
             } finally {
                 i++;
-            }
+            }*/
         }
     }
 
