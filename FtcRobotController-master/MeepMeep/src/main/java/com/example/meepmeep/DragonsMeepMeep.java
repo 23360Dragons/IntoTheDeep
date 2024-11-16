519974ce10569c150cbd73999fc956da397b9032
UnheardWolf344 <griffin3868@gmail.com>
2024-11-15 19:00:13 -0500

    NOT COMPLETE | Update MeepMeep for roadrunner 1

---
+++ FtcRobotController-master/MeepMeep/src/main/java/com/example/meepmeep/DragonsMeepMeep.java
@@ -0,0 +1,51 @@
+package com.example.meepmeep;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class DragonsMeepMeep {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(760);
+        int blueFace = 270;
+        Vector2d blueOne = new Vector2d(-12,62);
+        Vector2d blueTwo = new Vector2d(35,62);
+        Vector2d toBlueSide = new Vector2d(0,34);
+        int redFace = 90;
+        Vector2d redOne = new Vector2d(-35,-60);
+        Vector2d redTwo = new Vector2d(12,-60);
+        Vector2d toRedSide = new Vector2d(0, -33);
+        Pose2d blueBasket = new Pose2d(53,53,Math.toRadians(45));
+        Vector2d run = new Vector2d(10,43);
+        Vector2d backup = new Vector2d(0,43);
+        Pose2d redBasket = new Pose2d(-52, -52,Math.toRadians(225));
+        Pose2d redAscent = new Pose2d (-25,0,Math.toRadians(0));
+        Pose2d blueAscent = new Pose2d(25,0, Math.toRadians(180));
+        Pose2d blueObserve = new Pose2d(-57,60,Math.toRadians(90));
+        Pose2d redObserve = new Pose2d(57,-58,Math.toRadians(270));
+
+
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(redTwo, Math.toRadians(redFace)))
+                .waitSeconds(3)
+                .lineToY(toRedSide.y)
+                .waitSeconds(3)
+                .lineToYSplineHeading(redObserve.position.y, redObserve.heading)/*.lineToYSplineHeading(redObserve.position.y, redObserve.heading)*/
+                .waitSeconds(3)
+                .build());
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
