75a557fdcea5696e976a6877286d9ed3a00134d0
23360Dragons <23360Dragons@gmail.com>
2025-01-28 18:17:09 -0500

    Update Constants.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Constants.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Constants.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 public final class Constants {
     // Declare your constants as public, static, and final
     //Griffin says PPR (ticks/pulses per revolution) for our drive motors is 384.5 PPR
-    public static final double TICKS_PER_INCH = 384.5 / 3.7795;
+    public static final double TICKS_PER_INCH = 384.5 / 11.88;
     public static final double ROBOT_WIDTH = 18.0; // inches
     public static final double WHEEL_DIAMETER = 3.7795; // inches
     public static final String TEAM_NAME = "Dragons Anonymous";
