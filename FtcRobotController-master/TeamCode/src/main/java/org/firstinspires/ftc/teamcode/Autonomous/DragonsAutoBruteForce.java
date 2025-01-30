6a96dc440a1a0bf7a66d81b0e7185c0c3d2c8860
23360Dragons <23360Dragons@gmail.com>
2025-01-30 18:27:33 -0500

    Update DragonsAutoBruteForce.java

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DragonsAutoBruteForce.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DragonsAutoBruteForce.java
@@ -33,56 +33,55 @@ public class DragonsAutoBruteForce extends LinearOpMode {
 
 
         int starty = 0;
-//        while (opModeInInit()) {
-//            if (gamepad1.x) {
-//                starty = 1;
-//            } else if (gamepad1.b) {
-//                starty = 2;
-//            } else if (gamepad1.a) {
-//                starty = 3;
-//            } else if (gamepad1.y) {
-//                starty = 4;
-//            }
-//            switch (starty) {
-//                case 1:
-//                    //Actions.runBlocking(littleLarryLime.LarryLimeYellow());
-//                    telemetry.addLine("Starting Position Set To Blue, Basket Side. If inncorrect, please reselect");
-//                    telemetry.update();
-//                    break;
-//                case 2:
-//                    //Actions.runBlocking(littleLarryLime.LarryLimeYellow());
-//                    telemetry.addLine("Starting Position Set To Red, Basket Side. If inncorrect, please reselect");
-//                    telemetry.update();
-//                    break;
-//                case 3:
-//                    //Actions.runBlocking(littleLarryLime.LarryLimeBlues());
-//                    telemetry.addLine("Starting Position Set To Blue, Observation Zone Side. If inncorrect, please reselect");
-//                    telemetry.update();
-//                    break;
-//                case 4:
-//                    //Actions.runBlocking(littleLarryLime.LarryLimeRedTV());
-//                    telemetry.addLine("Starting Position Set To Red, Observation Zone Side. If inncorrect, please reselect");
-//                    telemetry.update();
-//                    break;
-//                default:
-//                    telemetry.addLine("Please select starting position! If not selected, the robot will not run during Auto.");
-//                    telemetry.update();
-//                    break;
-//            }
-//        }
+        while (opModeInInit()) {
+            if (gamepad1.x) {
+                starty = 1;
+            } else if (gamepad1.b) {
+                starty = 2;
+            } else if (gamepad1.a) {
+                starty = 3;
+            } else if (gamepad1.y) {
+                starty = 4;
+            }
+            switch (starty) {
+                case 1:
+                    //Actions.runBlocking(littleLarryLime.LarryLimeYellow());
+                    telemetry.addLine("Starting Position Set To Blue, Basket Side. If inncorrect, please reselect");
+                    telemetry.update();
+                    break;
+                case 2:
+                    //Actions.runBlocking(littleLarryLime.LarryLimeYellow());
+                    telemetry.addLine("Starting Position Set To Red, Basket Side. If inncorrect, please reselect");
+                    telemetry.update();
+                    break;
+                case 3:
+                    //Actions.runBlocking(littleLarryLime.LarryLimeBlues());
+                    telemetry.addLine("Starting Position Set To Blue, Observation Zone Side. If inncorrect, please reselect");
+                    telemetry.update();
+                    break;
+                case 4:
+                    //Actions.runBlocking(littleLarryLime.LarryLimeRedTV());
+                    telemetry.addLine("Starting Position Set To Red, Observation Zone Side. If inncorrect, please reselect");
+                    telemetry.update();
+                    break;
+                default:
+                    telemetry.addLine("Please select starting position! If not selected, the robot will not run during Auto.");
+                    telemetry.update();
+                    break;
+            }
+        }
 
         waitForStart();
-//        switch (starty) {
-//            case 1:
-//                robotMovement.strafe(84, true);
-//            case 2:
-//                robotMovement.strafe(84, true);
-//            case 3:
-//                robotMovement.strafe(48, true);
-//            case 4:
-//                robotMovement.strafe(48, true);
-
-//        }
-        robotMovement.strafe(200, true);
+        switch (starty) {
+            case 1:
+                robotMovement.strafe(84, true);
+            case 2:
+                robotMovement.strafe(84, true);
+            case 3:
+                robotMovement.strafe(48, true);
+            case 4:
+                robotMovement.strafe(48, true);
+        }
+        //robotMovement.strafe(200, true);
     }
 }
